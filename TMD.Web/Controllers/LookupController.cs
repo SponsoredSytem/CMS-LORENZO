b0c8a7e9e49dab5ea72d076d833da746f1266581
Syed Usman <usman@innostark.com>
2015-08-11 19:36:20 +0500

    City

---
+++ TMD.Web/Controllers/LookupController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TMD.Interfaces.IServices;
+using TMD.Web.ModelMappers;
+using TMD.Web.Models;
+
+namespace TMD.Web.Controllers
+{
+    public class LookupController : BaseController
+    {
+
+        private readonly ICityService cityService;
+
+        public LookupController(ICityService cityService)
+        {
+            this.cityService = cityService;
+        }
+        //
+        // GET: /Lookup/
+        public ActionResult City()
+        {
+            IEnumerable<City> cities = cityService.GetAllCities().Select(x => x.CreateFromServerToClient());
+            return View(cities);
+        }
+	}
+}
